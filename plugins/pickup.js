5687890a24eb077f1784fdfe67e3c6961eebddd5
Pkdriller <technexus613@gmail.com>
2025-07-17 10:50:02 +0300

    Update pickup.js

--- plugins/pickup.js
+++ plugins/pickup.js
@@ -11,31 +11,6 @@ cmd({
     react: "💌"
 }, async (conn, m, { sender }) => {
 
-    const quotedContact = {
-        key: {
-            fromMe: false,
-            participant: `0@s.whatsapp.net`,
-            remoteJid: "status@broadcast"
-        },
-        message: {
-            contactMessage: {
-                displayName: "Official NEXUS-XMD™",
-                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;NEXUS-XMD;;;\nFN:NEXUS-XMD\nitem1.TEL;waid=254700000000:+254 700 000000\nitem1.X-ABLabel:Official Bot\nEND:VCARD`
-            }
-        }
-    };
-
-    const contextInfo = {
-        mentionedJid: [sender],
-        forwardingScore: 999,
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363288304618280@newsletter',
-            newsletterName: 'NEXUS-XMD UPDATES',
-            serverMessageId: 107,
-        }
-    };
-
     const pickupLines = [
         "Are you a magician? Because whenever I look at you, everyone else disappears. ✨",
         "Do you have a map? I just got lost in your eyes. 🗺️👀",
@@ -49,15 +24,43 @@ cmd({
         "I must be a snowflake, because I’ve fallen for you. ❄️💘"
     ];
 
+    const quoteMsg = {
+        key: {
+            fromMe: false,
+            participant: `0@s.whatsapp.net`,
+            remoteJid: "status@broadcast"
+        },
+        message: {
+            contactMessage: {
+                displayName: "NEXUS-XMD Verified Bot",
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;NEXUS-XMD;;;\nFN:NEXUS-XMD\nitem1.TEL;waid=254700000000:+254700000000\nitem1.X-ABLabel:Official Bot\nEND:VCARD`
+            }
+        }
+    };
+
+    const contextInfo = {
+        mentionedJid: [sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: '120363288304618280@newsletter',
+            newsletterName: 'NEXUS-XMD UPDATES',
+            serverMessageId: 108
+        }
+    };
+
     const line = pickupLines[Math.floor(Math.random() * pickupLines.length)];
 
-    const response = `┏━━━━━━༺💌༻━━━━━━┓
+    const msg = `┏━━━༺💌༻━━━┓
 *✨ NEXUS PICKUP LINE ✨*
-┗━━━━━━━━━━━━━━━━━━━━┛
+┗━━━━━━━━━━━━━━┛
 
-"${line}"
+${line}
 
-💘 _Flirting powered by NEXUS-XMD Bot™_`
+💘 _Generated with love by NEXUS-XMD™_`;
 
-    await conn.sendMessage(m.chat, { text: response, contextInfo }, { quoted: quotedContact });
+    await conn.sendMessage(m.chat, {
+        text: msg,
+        contextInfo
+    }, { quoted: quoteMsg });
 });
